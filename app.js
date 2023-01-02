2952fb6c7920bffbf932501725155677415c198e
Pritam Dhar <84518701+pritamdhar6468@users.noreply.github.com>
2023-01-02 11:02:00 +0530

    Update app.js

---
+++ app.js
@@ -0,0 +1,3 @@
+const jokeContainer=document.getElementById("joke");
+const btn=document.getElementById("btn");
+const url="https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=single";
